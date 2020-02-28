d1702429524fc35bc64f83aab6def37a33b2cd16
travisalongi <50125469+travisalongi@users.noreply.github.com>
2020-02-27 17:39:21 -0800

    interpolate gps xy coords as function of time

--- change_sgy_header.py
+++ change_sgy_header.py
@@ -3,6 +3,7 @@
 """
 Created on Mon Jan 27 13:50:44 2020
 Update segy header with navigation file information that contains time & lat/lon
+processed all lines that are full lines, not turns *b.segy
 @author: talongi
 """
 import os 
@@ -13,14 +14,25 @@ import numpy as np
 import segyio as sgy
 import utm
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 from obspy.core import UTCDateTime
+from collections import Counter
 
+#set some function definitions
 def remove_decimal(number, places_after_decimal_to_retain = 2):
     """removes decimal place and truncates the new number"""
     n_list_strings = str(number).split('.')
     truncated_decimal = n_list_strings[1][:places_after_decimal_to_retain]
     new_number = int(n_list_strings[0] + truncated_decimal)
     return new_number
+
+def parametric_interpolation(x ,y, t, type = 'linear'):
+    """takes arrays of x, y, t parameterizes on t and returns interpolation
+    of f(x(t)) and f(y(t)) as a tuple"""
+    fx_t = interp1d(t,x, fill_value='extrapolate')
+    fy_t = interp1d(t,y, fill_value='extrapolate')    
+    return fx_t, fy_t
+ 
     
 ## import data
 nav = pd.read_csv('E:\\O199SC\\o199sc\\nav\\o-1-99-sc.061', 
@@ -28,14 +40,33 @@ nav = pd.read_csv('E:\\O199SC\\o199sc\\nav\\o-1-99-sc.061',
                        skiprows = 10,
                        names = ['datetime', 'lat', 'lon'])
 
+# convert nav to utc time
+nav['utc_datetime'] = [UTCDateTime(year = int(t[:4]), 
+                          julday = int(t[4:7]), 
+                          hour = int(t[7:9]), 
+                          minute = int(t[9:11]), 
+                          second = int(t[11:13])) for t in nav.datetime.astype(str).values]
+
+ #remove fraction of second from datetime
+nav['datetime'] = [int(time[:-1]) for time in nav.datetime.astype(str).values]
+
+# switch directories get file names
 data_dir = 'E:\\O199SC\\airgun'
 os.chdir(data_dir)
 files = glob.glob('*.sgy')
-cmap = plt.cm.viridis(np.linspace(0,1,len(files)))
+cmap = plt.cm.viridis(np.linspace(0,1,len(files))) #set color map for lines
+
+#initiate plot (part of sanity check)
+plt.figure(1,figsize = (12,12))
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Line map for O199SC')
 
 
 ## loop through files, change header
 for j, file in enumerate(files):
+    tic = time.time()
+    line_number = file.split('.')[0][-4:]
     f = sgy.open(file, 'r+', ignore_geometry = True)
     
     h = f.header #all trace headers
@@ -73,33 +104,49 @@ for j, file in enumerate(files):
         sec_list.append(ss)
                 
     date_trace = [int('19' + y + j + h + m + s) for y,j,h,m,s in zip(yr,jday,hr_list,mn_list,sec_list)]    
-    date_nav = [int(str(d)[:-1]) for d in nav.datetime]
+    date_trace_utc = [UTCDateTime(year = int('19' + y), 
+                                  julday = int(j), 
+                                  hour = int(h), 
+                                  minute = int(m), 
+                                  second = int(s)) for y,j,h,m,s in zip(yr,jday,hr_list,mn_list,sec_list)]
+
     
+    ##trim nav file for trace times
+    line_time_begin = date_trace_utc[0]
+    line_time_end = date_trace_utc[-1]
+    mask = (nav.utc_datetime >= line_time_begin) & (nav.utc_datetime <= line_time_end)
     
-    ## compare date/time from gps navigation to traces and populate source x&y location 
-    tic = time.time()
-    for i, date in enumerate(date_trace):
-        index_closest_time = np.abs(date - np.array(date_nav)).argmin() #gets index of closest time between trace and navigation
-        lat_near = nav.lat[index_closest_time]
-        lon_near = nav.lon[index_closest_time]
-        utm_coords = utm.from_latlon(lat_near, lon_near) #calculate utm coord b/c segy headers do not take floats
-        
-        easting = remove_decimal(utm_coords[0]) #remove decimal using function above
-        northing = remove_decimal(utm_coords[1])
+    #nav for this line
+    nav_line = nav[mask]
+    
+    #interpolate lat/lon as a function of trace time
+    f_lon, f_lat = parametric_interpolation(nav_line.lon, nav_line.lat, nav_line.datetime)
+    trace_lon = f_lon(date_trace)
+    trace_lat = f_lat(date_trace)
+    
+    #convert trace location to utm
+    utm_coords = utm.from_latlon(trace_lat, trace_lon)
+    
+    #populate src x&y headers
+    for i, (x,y) in enumerate(zip(utm_coords[0], utm_coords[1])):
+        easting = remove_decimal(x)
+        northing = remove_decimal(y)
         
         h[i][sgy.TraceField.SourceX] = easting #assign source locations
         h[i][sgy.TraceField.SourceY] = northing
         h[i][sgy.TraceField.SourceGroupScalar] = -100 #scaler that recomputes to account for moving decimal
+        
+    #sanity checks
+    plt.plot(nav_line.lon, nav_line.lat, 'k.')
+    plt.plot(trace_lon, trace_lat, '.', color = cmap[j])
+    plt.text(trace_lon[0], trace_lat[0], line_number)
+    sample_rate_counts = Counter(np.diff(np.unique(date_trace_utc)))
+    print('Line #',line_number,'**sample rates =' , sample_rate_counts)
+    
     toc = time.time()
-    print(file, ' run time = ',toc-tic)
-    
-    byte_number = sgy.TraceField.SourceX
-    src_x = [h[i][byte_number] for i in range(n)]
-    src_y = [h[i][77] for i in range(n)]
-    
-    plt.figure(1)
-    plt.plot(src_x, src_y, label = file)
-    plt.axis('equal')
-    plt.legend()
+    print(file, ' run time = ',toc-tic, '\n')
     
     f.close
+    
+    
+
